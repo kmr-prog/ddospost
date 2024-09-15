97a90cca119c1b6d7945f5d0a20fc2c6c52af022
kmr-prog <botzputu@gmail.com>
2024-09-15 12:31:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let ddosin = require("./lol.js")
-ddosin(`fallxd_7811`, `bang aku mau sc gratis`)
+
+const requestData = {
+    message: 'halo, siapa kamu?',
+    characterId: 'usiilspp875cj9s',
+    sessionName: 'shannz'
+  };
+
+ddosin(`https://cai.shannmoderz.xyz/v1/completions`, requestData)
